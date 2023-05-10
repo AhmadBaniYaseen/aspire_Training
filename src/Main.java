4533e8398574357c580c3e53a0139a36dcf59217
AhmadBaniYaseen <ahmadbaniyassen8@gmail.com>
2023-05-11 02:15:31 +0300

    Assignment #4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+
+import assignments4.Course;
+import assignments4.Gender;
+import assignments4.Student;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
---
+++ src/assignments4/Gender.java
@@ -0,0 +1,5 @@
+package assignments4;
+
+public enum Gender {
+    MALE,FEMALE
+}
